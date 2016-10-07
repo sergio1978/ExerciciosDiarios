00fe7f7ca9314dfa81df1a803476c4a0cf973de0
UserPL022Pc14 <UserPL022Pc14@SLFRPL022PC14.ensino.atec.local>
2016-10-07 15:41:55 +0100

    segundo

---
+++ src/exercicios071016/Treino.java
@@ -0,0 +1,30 @@
+package exercicios071016;
+
+import java.util.Scanner;
+
+public class Treino {
+
+	public Treino() {
+		// TODO Auto-generated constructor stub
+	
+		//exercicio1
+	}
+	
+	private void exercicio1() {
+		
+		int num1 = (new Scanner(System.in)).nextInt();
+		int num2 = (new Scanner(System.in)).nextInt();		
+		
+		
+		if (num1 > num2) {
+			System.out.println("O numero 1 eh o maior");
+		} else {
+			System.out.println("O numero 2 eh o maior");
+		}
+		
+	}
+	
+	
+	
+	}
+	
